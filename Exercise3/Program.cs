1e78e46afadac3c89157e338f3ebf94f4efd1aaa
Dario-Miccinelli <poduzzo@hotmail.com>
2024-09-20 15:54:25 +0200

    Exercise 3 setup

---
+++ Exercise3/Program.cs
@@ -0,0 +1,17 @@
+﻿using Relewise.Client.DataTypes;
+
+// Exercise 3: Download Raw Product Data and Map to Relewise Products
+// - Write a C# Class, that implements the IJob -interface (see Appendix-section)
+// - The Execute(…)-method should:
+// o Download data from this URL:
+// ▪ https://cdn.relewise.com/academy/productdata/raw
+// o Parse the raw data and map each row into an instance of
+// Relewise.Client.DataTypes.Product (available on NuGet: Install-Package
+// Relewise.Client)
+// ▪ The minimum properties that you should map are:
+// • ProductId should map to string Id
+// • ProductName should map to Multilingual DisplayName
+// • List Price should map to MultiCurrency ListPrice
+// • Sales Price should map to MultiCurrency SalesPrice
+// o The method, which returns a string, should return a message containing a count of how
+// many products got mapped.
